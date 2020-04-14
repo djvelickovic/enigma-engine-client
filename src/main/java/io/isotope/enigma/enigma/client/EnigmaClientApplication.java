c219282c9467196707909193e2a3c05f11ec7f94
djovel <djvelickovic234@gmail.com>
2020-04-14 22:19:27 +0200

    Clean client project.

---
+++ src/main/java/io/isotope/enigma/enigma/client/EnigmaClientApplication.java
@@ -0,0 +1,13 @@
+package io.isotope.enigma.enigma.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/isotope/enigma/enigma/client/EnigmaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.isotope.enigma.enigma.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
